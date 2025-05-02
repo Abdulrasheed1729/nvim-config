a70eb5693f7bd970bbdba4fd5e2e36e95768ab1c
Abdulrasheed Fawole <fawomath@gmail.com>
2025-05-03 00:17:33 +0100

    config till lsp

---
+++ init.lua
@@ -0,0 +1,27 @@
+require("config.lazy")
+
+
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+local cool_stuff = 5
+
+print(cool_stuff)
+print(cool_stuff + 1)
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
---
+++ lua/config/plugins/lsp.lua
@@ -0,0 +1,109 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    -- bridges the gap between mason and lspconfig
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            -- Load the wezterm types when the `wezterm` module is required
+            -- Needs `justinsgithub/wezterm-types` to be installed
+            { path = "wezterm-types",      mods = { "wezterm" } },
+          },
+        },
+      },
+      -- Support for dart hot-reload on save
+      { "RobertBrunhage/dart-tools.nvim" },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+
+      require("mason").setup()
+
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          -- dart sdk ships with LSP
+          "lua_ls",
+        },
+      })
+
+
+      -- Lua Set up
+      lspconfig.lua_ls.setup {}
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('my.lsp', {}),
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+          if not client then return end
+
+          if client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+
+
+      -- Zig Language Server Set up
+      lspconfig.zls.setup {}
+
+      vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
+      -- vim.lsp.enable('dartls')
+
+      -- Dart Set up
+      local dartExcludedFolders = {
+        vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+        vim.fn.expand("$HOME/.pub-cache"),
+        vim.fn.expand("/opt/homebrew/"),
+        vim.fn.expand("$HOME/tools/flutter/"),
+      }
+
+
+      lspconfig.dartls.setup {
+        cmd = {
+          "dart",
+          "language-server",
+          "--protocol=lsp",
+        },
+        filetypes = { "dart" },
+        init_options = {
+          onlyAnalyzeProjectsWithOpenFiles = false,
+          suggestFromUnimportedLibraries = true,
+          closingLabels = true,
+          outline = true,
+          flutterOutline = true,
+        },
+        settings = {
+          dart = {
+            analysisExcludedFolders = dartExcludedFolders,
+            updateImportsOnRename = true,
+            completeFunctionCalls = true,
+            showTodos = true,
+          }
+        }
+      }
+    end,
+  }
+}
