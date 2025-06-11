4bcac6f97a8f75d2de5f04fdffac40fcada93549
Abdulrasheed Fawole <fawomath@gmail.com>
2025-06-11 20:53:16 +0100

    setup notify

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,19 +1,21 @@
 return {
   {
-    'echasnovski/mini.nvim',
-    version = '*',
+    "echasnovski/mini.nvim",
+    version = "*",
     config = function()
       -- Setup statusline
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = true }
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = true })
 
       -- Set up mini.diff
-      local diff = require('mini.diff')
-      diff.setup {}
+      local diff = require("mini.diff")
+      diff.setup({})
 
       -- Set up mini.notify
-      local notify = require('mini.notify')
-      notify.setup {}
-    end
-  }
+      local notify = require("mini.notify")
+      notify.setup({})
+      local opts = { ERROR = { duration = 5000 } }
+      vim.notify = notify.make_notify(opts)
+    end,
+  },
 }
