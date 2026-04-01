4ea60256fa868a3d0ca240d2214266be0f75c777
Abdulrasheed Fawole <fawomath@gmail.com>
2026-04-01 12:21:10 +0200

    remove lspconfig deprecated code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -270,8 +270,7 @@ return {
         end,
       },
     })
-    require("lspconfig").dafny.setup({})
-    -- local lspconfig = require("lspconfig")
+    vim.lsp.enable("dafny") -- local lspconfig = require("lspconfig")
 
     -- -- Dart Set up
     -- local dartExcludedFolders = {
